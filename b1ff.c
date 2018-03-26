e7d45c7c896b7a2199913209e2b38f682c60b18a
masinfund <tql3384@163.com>
2018-03-26 16:18:24 +0800

    modified

--- b1ff.c
+++ b1ff.c
@@ -3,81 +3,8 @@
 # define SIZE 100
 
 int main(void)
-
 {
-	int ch , m[SIZE] = {0} , i = 0  , amount ;
-
-	printf("Enter message :  ");
-
-	// ctrl + z   回车键  ctrl + z  要不然会读入'\n'
-
-	while( ( ch = getchar() ) != EOF ) {
+	printf("h") ;
 	
-		ch = toupper(ch) ;
-
-		m[i] = ch ;
-
-		i++ ;
-	
-	}
-
-	amount = i ;
-
-	printf("In B1FF-speak : ") ;
-
-	for ( i = 0 ;  i < amount ; i++ ){
-		
-		switch( m[i] ){
-		
-			case 'A' :
-
-				printf("4");
-
-				break ;
-
-			case 'B' :
-
-				printf("8");
-
-				break ;
-
-			case 'E' :
-
-				printf("3") ;
-
-				break ;
-
-			case 'I' :
-
-				printf("1") ;
-
-				break ;
-
-			case 'O' :
-
-				printf("0") ;
-
-				break ;
-
-			case 'S' :
-
-				printf("5") ;
-
-				break ;
-
-			default :
-
-				printf("%c",m[i] ) ;
-
-				break ;
-		
-		}
-
-	
-	}
-
-	printf("!!!!!!!!!!\n") ;
-
-
 	return 0 ;
 }
